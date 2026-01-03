c3796327a69a80759bb5d2368248909a65fe9c10
Alfas Salim <alfassalim123@gmail.com>
2026-01-03 15:54:56 +0530

    django test work

---
+++ python/python-data_encapsulation.py
@@ -0,0 +1,21 @@
+# Data Encapsulation
+class personalComputer:
+    def __init__(self):
+        self.maxComputerPrice = 20000
+        
+    def mySell(self):
+        print(f"Selling price is {self.maxComputerPrice}")
+        
+    def setMaxComputerPrice(self, price):
+        self.maxComputerPrice = price
+        
+pc = personalComputer()
+pc.mySell()
+
+# change the price
+pc.maxComputerPrice = 30000
+pc.mySell()
+
+# using setter function
+pc.setMaxComputerPrice(40000)
+pc.mySell()
--- python/python-oops.py
+++ python/python-oops.py
@@ -2,7 +2,7 @@ class myBird:
     def __init__(self):
         print("myBird class constructor is executing")
         
-    def type(self):
+    def whatType(self):
         print('I am a bird')
     
     def canFly(self):
@@ -22,3 +22,32 @@ class myParrot:
         
     def canSing(self, thisSong):
         print(f"{self.name} can sing {thisSong}")
+        
+class myPenguin(myBird):
+    def __init__(self):
+        super().__init__()
+        print("myPenguin is ready")
+        
+    def whoIsThis(self):
+        print("I am Penguin")
+        
+    def canRun(self):
+        print("Penguin can run faster")
+        
+mp1 = myParrot("MyParrot1", 10)
+mp2 = myParrot("MyParrot2", 15)
+
+# accessing class attributes
+print(f"mp1 is a {mp1.__class__.species}")
+print(f"mp2 is a {mp1.__class__.species}")
+
+# accessing instance attributes
+print(f"{mp1.name} is {mp1.age} years old")
+print(f"{mp2.name} is {mp2.age} years old")
+print(mp1.canSing("Chirp"))
+
+pg1 = myPenguin()
+pg1.whoIsThis()
+pg1.canSwim()
+pg1.canRun()
+        
